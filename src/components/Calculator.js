bb84e3915a09e0b2e0f8772b5dbb2b94798ee9d4
MasterOfOrigin <michaelbrendanmahon@gmail.com>
2019-09-05 22:43:03 -0500

    html blocks template for calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,10 +3,34 @@ import React from 'react';
 export class Calculator extends React.Component {
   render() {
     return (
-      <div className="container">
-        <h1>
-          CALCULATOR
-        </h1>
+      <div className="calculator container rounded" id="calculator">
+        <div className="row display text-white text-lg-right align-text-bottom">
+          <div className="rounded bg-secondary col-12">HELLO</div>
+        </div>
+        <div className="buttons">
+          <div className="row">
+            <div className="bg-secondary col-9 button">HELLO</div>
+            <div className="bg-secondary col-3 button">HI</div>
+          </div>
+          <div className="row">
+            <div className="bg-secondary col-3 button">HI</div>
+            <div className="bg-secondary col-3 button">HI</div>
+            <div className="bg-secondary col-3 button">HI</div>
+            <div className="bg-secondary col-3 button">HI</div>
+          </div>
+          <div className="row">
+            <div className="bg-secondary col-3 button">HI</div>
+            <div className="bg-secondary col-3 button">HI</div>
+            <div className="bg-secondary col-3 button">HI</div>
+            <div className="bg-secondary col-3 button">HI</div>
+          </div>
+          <div className="row">
+            <div className="bg-secondary col-3 button">HI</div>
+            <div className="bg-secondary col-3 button">HI</div>
+            <div className="bg-secondary col-3 button">HI</div>
+            <div className="bg-secondary col-3 button">HI</div>
+          </div>
+        </div>
       </div>
     )
   }
